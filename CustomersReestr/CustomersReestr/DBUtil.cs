0dd7e53011da217de511c24759ceeebb3850087d
Darth Raven <zarrass@mail.ru>
2017-12-12 09:55:46 +0700

    Check.Connection()

--- CustomersReestr/CustomersReestr/DBUtil.cs
+++ CustomersReestr/CustomersReestr/DBUtil.cs
@@ -5,15 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows;
+using System.Configuration;
 
 namespace CustomersReestr
 {
     class DBUtil
        
     {
+        
+        internal static string GetConnectionString()
+        {
+             
+            string returnValue = null;
+
+            
+            ConnectionStringSettings settings =
+            ConfigurationManager.ConnectionStrings["CustomersReestr.Properties.Settings.CustomersConnectionString"];
+
+            
+            if (settings != null)
+                returnValue = settings.ConnectionString;
+
+            return returnValue;
+        }
         public static void CheckConnection()
         {
-            MessageBox.Show("DB Connected!!");
+            SqlConnection cnn;
+            cnn = new SqlConnection(GetConnectionString());
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
         }
     }
+
 }
