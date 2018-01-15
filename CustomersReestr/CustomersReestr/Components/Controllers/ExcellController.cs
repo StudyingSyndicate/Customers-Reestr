21bcb266879293eca29847183ef4fb88e11f9372
god <zarrass@mail.ru>
2018-01-15 15:01:01 +0700

    Add ExelController. Neeed tests

--- CustomersReestr/CustomersReestr/Components/Controllers/ExcellController.cs
+++ CustomersReestr/CustomersReestr/Components/Controllers/ExcellController.cs
@@ -5,100 +5,250 @@ using System.Data.Objects;
 using System.Data.EntityClient;
 using System.Linq;
 using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using CustomersReestr.Components.Models;
+using System.Diagnostics;
 
 namespace CustomersReestr.Components.Controllers
 {
     class ExcellController
     {
-        public void EntityToExcelSheet(string excelFilePath, string sheetName, IQueryable result, ObjectContext ctx)
+        public static bool CreateExcelDocument<T>(List<T> list, string xlsxFilePath)
         {
-            Excel.Application oXL;
-            Excel.Workbook oWB;
-            Excel.Worksheet oSheet;
-            Excel.Range oRange;
+            DataSet ds = new DataSet();
+            ds.Tables.Add(ListToDataTable(list));
+
+            return CreateExcelDocument(ds, xlsxFilePath);
+        }
+
+        public static DataTable ListToDataTable<T>(List<T> list)
+        {
+            DataTable dt = new DataTable();
+
+            foreach (PropertyInfo info in typeof(T).GetProperties())
+            {
+                dt.Columns.Add(new DataColumn(info.Name, GetNullableType(info.PropertyType)));
+            }
+            foreach (T t in list)
+            {
+                DataRow row = dt.NewRow();
+                foreach (PropertyInfo info in typeof(T).GetProperties())
+                {
+                    if (!IsNullableType(info.PropertyType))
+                        row[info.Name] = info.GetValue(t, null);
+                    else
+                        row[info.Name] = (info.GetValue(t, null) ?? DBNull.Value);
+                }
+                dt.Rows.Add(row);
+            }
+            return dt;
+        }
+        private static Type GetNullableType(Type t)
+        {
+            Type returnType = t;
+            if (t.IsGenericType && t.GetGenericTypeDefinition().Equals(typeof(Nullable<>)))
+            {
+                returnType = Nullable.GetUnderlyingType(t);
+            }
+            return returnType;
+        }
+        private static bool IsNullableType(Type type)
+        {
+            return (type == typeof(string) ||
+                    type.IsArray ||
+                    (type.IsGenericType &&
+                     type.GetGenericTypeDefinition().Equals(typeof(Nullable<>))));
+        }
+        public static bool CreateExcelDocument(DataTable dt, string xlsxFilePath)
+        {
+            DataSet ds = new DataSet();
+            ds.Tables.Add(dt);
+            bool result = CreateExcelDocument(ds, xlsxFilePath);
+            ds.Tables.Remove(dt);
+            return result;
+        }
+        public static bool CreateExcelDocument(DataSet ds, string excelFilename)
+        {
             try
             {
-                // Start Excel and get Application object.
-                oXL = new Excel.Application();
-
-                // Set some properties
-                oXL.Visible = true;
-                oXL.DisplayAlerts = false;
-
-                // Get a new workbook. 
-                oWB = oXL.Workbooks.Add(Missing.Value);
-
-                // Get the active sheet 
-                oSheet = (Excel.Worksheet)oWB.ActiveSheet;
-                oSheet.Name = sheetName;
-
-                // Process the DataTable
-                // BE SURE TO CHANGE THIS LINE TO USE *YOUR* DATATABLE 
-                DataTable dt = EntityToDataTable(result, ctx);
-
-                int rowCount = 1;
-                foreach (DataRow dr in dt.Rows)
+                using (SpreadsheetDocument document = SpreadsheetDocument.Create(excelFilename, SpreadsheetDocumentType.Workbook))
                 {
-                    rowCount += 1;
-                    for (int i = 1; i < dt.Columns.Count + 1; i++)
-                    {
-                        // Add the header the first time through 
-                        if (rowCount == 2)
-                            oSheet.Cells[1, i] = dt.Columns[i - 1].ColumnName;
-                        oSheet.Cells[rowCount, i] = dr[i - 1].ToString();
-                    }
+                    WriteExcelFile(ds, document);
                 }
-
-                // Resize the columns 
-                oRange = oSheet.Range[oSheet.Cells[1, 1], oSheet.Cells[rowCount, dt.Columns.Count]];
-                oRange.Columns.AutoFit();
-
-                // Save the sheet and close 
-                oSheet = null;
-                oRange = null;
-                oWB.SaveAs(excelFilePath, Excel.XlFileFormat.xlWorkbookNormal, Missing.Value,
-                  Missing.Value, Missing.Value, Missing.Value,
-                  Excel.XlSaveAsAccessMode.xlExclusive, Missing.Value,
-                  Missing.Value, Missing.Value, Missing.Value);
-                oWB.Close(Missing.Value, Missing.Value, Missing.Value);
-                oWB = null;
-                oXL.Quit();
+                Trace.WriteLine("Successfully created: " + excelFilename);
+                return true;
             }
             catch (Exception ex)
             {
-                throw ex;
+                Trace.WriteLine("Failed, exception thrown: " + ex.Message);
+                return false;
+            }
+
+        }
+        private static void WriteExcelFile(DataSet ds, SpreadsheetDocument spreadsheet)
+        {
+            //  Create the Excel file contents.  This function is used when creating an Excel file either writing 
+            //  to a file, or writing to a MemoryStream.
+            spreadsheet.AddWorkbookPart();
+            spreadsheet.WorkbookPart.Workbook = new DocumentFormat.OpenXml.Spreadsheet.Workbook();
+
+            //  My thanks to James Miera for the following line of code (which prevents crashes in Excel 2010)
+            spreadsheet.WorkbookPart.Workbook.Append(new BookViews(new WorkbookView()));
+
+            //  If we don't add a "WorkbookStylesPart", OLEDB will refuse to connect to this .xlsx file !
+            WorkbookStylesPart workbookStylesPart = spreadsheet.WorkbookPart.AddNewPart<WorkbookStylesPart>("rIdStyles");
+            Stylesheet stylesheet = new Stylesheet();
+            workbookStylesPart.Stylesheet = stylesheet;
+
+            //  Loop through each of the DataTables in our DataSet, and create a new Excel Worksheet for each.
+            uint worksheetNumber = 1;
+            foreach (DataTable dt in ds.Tables)
+            {
+                //  For each worksheet you want to create
+                string workSheetID = "rId" + worksheetNumber.ToString();
+                string worksheetName = dt.TableName;
+
+                WorksheetPart newWorksheetPart = spreadsheet.WorkbookPart.AddNewPart<WorksheetPart>();
+                newWorksheetPart.Worksheet = new DocumentFormat.OpenXml.Spreadsheet.Worksheet();
+
+                // create sheet data
+                newWorksheetPart.Worksheet.AppendChild(new DocumentFormat.OpenXml.Spreadsheet.SheetData());
+
+                // save worksheet
+                WriteDataTableToExcelWorksheet(dt, newWorksheetPart);
+                newWorksheetPart.Worksheet.Save();
+
+                // create the worksheet to workbook relation
+                if (worksheetNumber == 1)
+                    spreadsheet.WorkbookPart.Workbook.AppendChild(new DocumentFormat.OpenXml.Spreadsheet.Sheets());
+
+                spreadsheet.WorkbookPart.Workbook.GetFirstChild<DocumentFormat.OpenXml.Spreadsheet.Sheets>().AppendChild(new DocumentFormat.OpenXml.Spreadsheet.Sheet()
+                {
+                    Id = spreadsheet.WorkbookPart.GetIdOfPart(newWorksheetPart),
+                    SheetId = (uint)worksheetNumber,
+                    Name = dt.TableName
+                });
+
+                worksheetNumber++;
+            }
+
+            spreadsheet.WorkbookPart.Workbook.Save();
+        }
+        private static void WriteDataTableToExcelWorksheet(DataTable dt, WorksheetPart worksheetPart)
+        {
+            var worksheet = worksheetPart.Worksheet;
+            var sheetData = worksheet.GetFirstChild<SheetData>();
+
+            string cellValue = "";
+
+            //  Create a Header Row in our Excel file, containing one header for each Column of data in our DataTable.
+            //
+            //  We'll also create an array, showing which type each column of data is (Text or Numeric), so when we come to write the actual
+            //  cells of data, we'll know if to write Text values or Numeric cell values.
+            int numberOfColumns = dt.Columns.Count;
+            bool[] IsNumericColumn = new bool[numberOfColumns];
+
+            string[] excelColumnNames = new string[numberOfColumns];
+            for (int n = 0; n < numberOfColumns; n++)
+                excelColumnNames[n] = GetExcelColumnName(n);
+
+            //
+            //  Create the Header row in our Excel Worksheet
+            //
+            uint rowIndex = 1;
+
+            var headerRow = new Row { RowIndex = rowIndex };  // add a row at the top of spreadsheet
+            sheetData.Append(headerRow);
+
+            for (int colInx = 0; colInx < numberOfColumns; colInx++)
+            {
+                DataColumn col = dt.Columns[colInx];
+                AppendTextCell(excelColumnNames[colInx] + "1", col.ColumnName, headerRow);
+                IsNumericColumn[colInx] = (col.DataType.FullName == "System.Decimal") || (col.DataType.FullName == "System.Int32");
+            }
+
+            //
+            //  Now, step through each row of data in our DataTable...
+            //
+            double cellNumericValue = 0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                // ...create a new row, and append a set of this row's data to it.
+                ++rowIndex;
+                var newExcelRow = new Row { RowIndex = rowIndex };  // add a row at the top of spreadsheet
+                sheetData.Append(newExcelRow);
+
+                for (int colInx = 0; colInx < numberOfColumns; colInx++)
+                {
+                    cellValue = dr.ItemArray[colInx].ToString();
+
+                    // Create cell with data
+                    if (IsNumericColumn[colInx])
+                    {
+                        //  For numeric cells, make sure our input data IS a number, then write it out to the Excel file.
+                        //  If this numeric value is NULL, then don't write anything to the Excel file.
+                        cellNumericValue = 0;
+                        if (double.TryParse(cellValue, out cellNumericValue))
+                        {
+                            cellValue = cellNumericValue.ToString();
+                            AppendNumericCell(excelColumnNames[colInx] + rowIndex.ToString(), cellValue, newExcelRow);
+                        }
+                    }
+                    else
+                    {
+                        //  For text cells, just write the input data straight out to the Excel file.
+                        AppendTextCell(excelColumnNames[colInx] + rowIndex.ToString(), cellValue, newExcelRow);
+                    }
+                }
             }
         }
 
-        public DataTable EntityToDataTable(IQueryable result, ObjectContext ctx)
+        private static void AppendTextCell(string cellReference, string cellStringValue, Row excelRow)
         {
-            try
-            {
-                EntityConnection conn = ctx.Connection as EntityConnection;
-                using (SqlConnection SQLCon = new SqlConnection(conn.StoreConnection.ConnectionString))
-                {
-                    ObjectQuery query = result as ObjectQuery;
-                    using (SqlCommand Cmd = new SqlCommand(query.ToTraceString(), SQLCon))
-                    {
-                        foreach (var param in query.Parameters)
-                        {
-                            Cmd.Parameters.AddWithValue(param.Name, param.Value);
-                        }
-                        using (SqlDataAdapter da = new SqlDataAdapter(Cmd))
-                        {
-                            using (DataTable dt = new DataTable())
-                            {
-                                da.Fill(dt);
-                                return dt;
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            //  Add a new Excel Cell to our Row 
+            Cell cell = new Cell() { CellReference = cellReference, DataType = CellValues.String };
+            CellValue cellValue = new CellValue();
+            cellValue.Text = cellStringValue;
+            cell.Append(cellValue);
+            excelRow.Append(cell);
+        }
+
+        private static void AppendNumericCell(string cellReference, string cellStringValue, Row excelRow)
+        {
+            //  Add a new Excel Cell to our Row 
+            Cell cell = new Cell() { CellReference = cellReference };
+            CellValue cellValue = new CellValue();
+            cellValue.Text = cellStringValue;
+            cell.Append(cellValue);
+            excelRow.Append(cell);
+        }
+
+        private static string GetExcelColumnName(int columnIndex)
+        {
+            //  Convert a zero-based column index into an Excel column reference  (A, B, C.. Y, Y, AA, AB, AC... AY, AZ, B1, B2..)
+            //
+            //  eg  GetExcelColumnName(0) should return "A"
+            //      GetExcelColumnName(1) should return "B"
+            //      GetExcelColumnName(25) should return "Z"
+            //      GetExcelColumnName(26) should return "AA"
+            //      GetExcelColumnName(27) should return "AB"
+            //      ..etc..
+            //
+            if (columnIndex < 26)
+                return ((char)('A' + columnIndex)).ToString();
+
+            char firstChar = (char)('A' + (columnIndex / 26) - 1);
+            char secondChar = (char)('A' + (columnIndex % 26));
+
+            return string.Format("{0}{1}", firstChar, secondChar);
         }
     }
+
+
+       
+          
 }
